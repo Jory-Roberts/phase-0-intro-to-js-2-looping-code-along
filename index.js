dffaf07019ca5a353bb1d25e5f2e38bdd2c296c6
Jory Roberts <robertsjory13@gmail.com>
2023-03-01 06:25:10 -0600

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+
+    }
+
+    return messages;
+
+}
+
+function countDown() {
+    let count = 11;
+    while (count > 0) {
+        count--;
+        
+        console.log(count);
+    }
+}
+
